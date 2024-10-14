3f6f613243eddab93a5de1bfa2180af281100356
Faiz <sheikhfaiz7861@gmail.com>
2024-10-14 20:44:59 +0530

    First

--- src/components/WhetherDisplay.js
+++ src/components/WhetherDisplay.js
@@ -5,15 +5,15 @@ const WhetherDisplay = function (props) {
   const { temprature, conditions } = data;
 
   const dataStyle = {
-    color: temprature < 20 ? "red" : "blue",
+    color: temprature > 20 ? "red" : "blue",
   };
 
   return (
     <div>
       <p>
-        Temprature : <span style={dataStyle}>{temprature}</span>
+        Temprature: <span style={dataStyle}>{temprature}</span>
       </p>
-      <p>conditions : {conditions}</p>
+      <p>Conditions: {conditions}</p>
     </div>
   );
 };
